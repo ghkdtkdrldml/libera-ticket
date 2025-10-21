0d4fa137d9c5f96424d4d7b5b888642a92467591
sangki <ghkdtkdrldml@gmail.com>
2025-10-22 00:14:34 +0900

    이미지 수정

--- src/main/java/com/libera/ticket/web/PageController.java
+++ src/main/java/com/libera/ticket/web/PageController.java
@@ -11,7 +11,7 @@ public class PageController {
     public String about(Model model) {
         // 필요하면 이미지 경로를 동적으로 주입
          model.addAttribute("aboutImg1", "/img/Libera_image1.jpg");
-         model.addAttribute("aboutImg2", "/img/Libera_image1.jpg");
+         model.addAttribute("aboutImg2", "/img/Libera_image2.jpg");
         return "about"; // src/main/resources/templates/about.html
     }
 }
